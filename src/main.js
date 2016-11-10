2d84f8ceebc9058fdf22eb1c37b5a64aecc896b6
ninayanez <nina@theblacksea.cc>
2016-11-10 13:24:43 -0800

    force window size

--- src/main.js
+++ src/main.js
@@ -1,45 +1,37 @@
-const electron = require('electron')
-// Module to control application life.
+import url from 'url'
+import path from 'path'
+import electron from 'electron'
+import imageToPlate from './imageToPlate.js'
 const app = electron.app
-// Module to create native browser window.
+const ipc = electron.ipcMain
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+ipc.on('data', function (event, data) {
+  if (data.file) imageToPlate(data, (s) => {
+    s.on('data', (d) => {
+      if (d.preview) event.sender.send('data', d)
+    })
+  })
+})
 
-  // and load the index.html of the app.
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 420, height: 320, resizeable: false})
+
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname.replace('dist',''), 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
