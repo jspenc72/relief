130485270993428222520511dc7d1b9132373d0e
ninayanez <nina@theblacksea.cc>
2016-11-11 00:15:58 -0800

    add new input handler and pass to imageToPlate, use imageData and canvas element to render images

--- src/renderer.js
+++ src/renderer.js
@@ -2,37 +2,52 @@ import electron from 'electron'
 import _ from 'underscore'
 import yaml from 'yamljs'
 import imageToPlate from './imageToPlate.js'
+import path from 'path'
 import proc from 'child_process'
 
-// const ipc = electron.ipcRenderer
-let img = null
-let scale = null
+let file = null
+const spinner = document.querySelector('.spinner')
+const cvs = document.createElement('canvas')
+const ctx = cvs.getContext('2d')
+document.body.appendChild(cvs)
+const log = document.querySelector('.status span')
 
-  
-const log = document.querySelector('.log')
 document.addEventListener('dragover', (e) => {e.preventDefault()}, false)
 document.addEventListener('drop', (e) => { 
   e.preventDefault()
 	if (e.dataTransfer.files.length === 1) {
-    const file = e.dataTransfer.files[0]
+    file = e.dataTransfer.files[0]
+    inputHandler('preview '+file.path)
     imageToPlate({file:file.path}, inputHandler)
 	}
 	else return false
 },false)
 
 function inputHandler (d) {
-  d = d.split(' ')
-  if (d[0] === 'prog') return
-  if (d[0] === 'open') { 
-    proc.exec(d[1]+' '+d[2], (e,se,so) => { console.log(e,se,so) })
+  spinner.style.opacity = 0
+  if (d.preview) {
+    const data = new ImageData(
+      new Uint8ClampedArray(d.preview.data),
+      d.preview.shape[0],
+      d.preview.shape[1]
+    )
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
+    cvs.width = data.width
+    cvs.height = data.height
+    cvs.style.marginLeft = -(data.width/2) + 'px' 
+    cvs.style.marginTop = -(data.height/2) + 'px'
+    ctx.putImageData(data,0,0)
   }
-  if (d[0] === 'preview') {
-    if (!img) img = document.createElement('img')
-    img.src = d[1]
-    img.onload = (e) => {
-      img.style.marginLeft = -parseInt(img.width/2)+'px'
-      img.style.marginTop = -(parseInt(img.height/2))+'px'
-      document.body.appendChild(img)
-    }
-  } else log.innerHTML = d[1]
+  if (d.open) {
+    log.innerHTML = 'Saved STL to '+path.dirname(file.path)
+    proc.exec('open '+path.dirname(d.open), (e,se,so) => { 
+       console.log(e,se,so) 
+       // if (!e) log.innerHTML = 'drop an image onto this window!'  
+    })
+  }
+  if (d.log) {
+    if (d.log.match('exporting'))
+      spinner.style.opacity = 1
+    log.innerHTML = d.log
+  }
 }
