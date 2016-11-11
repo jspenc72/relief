e84277ad0f36b3c783c0b60f02d59c95af262b5f
ninayanez <nina@theblacksea.cc>
2016-11-11 00:12:14 -0800

    disable window resizing, and remove ipc

--- src/main.js
+++ src/main.js
@@ -2,20 +2,14 @@ import url from 'url'
 import path from 'path'
 import electron from 'electron'
 import imageToPlate from './imageToPlate.js'
+import proc from 'child_process'
 const app = electron.app
-const ipc = electron.ipcMain
 const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
-ipc.on('data', function (event, data) {
-  if (data.file) imageToPlate(data, (s) => {
-    s.on('data', (d) => { event.sender.send('data', d) })
-  })
-})
-
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 420, height: 320, resizeable: false})
+  mainWindow = new BrowserWindow({width: 420, height: 320, resizable: false})
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname.replace('dist',''), 'index.html'),
@@ -30,18 +24,5 @@ function createWindow () {
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('window-all-closed', function () { app.quit() })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
